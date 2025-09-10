70bffb690a9bdd8b51c49dd477d025c6352621fd
AdrienW86 <adrien_weiss@outlook.fr>
2025-09-10 13:02:45 +0200

    mise en ligne

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
 import { Baloo_2 } from "next/font/google";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Script from "next/script"; // ✅ Import Script
 import "./globals.css";
 
 const baloo = Baloo_2({
-  variable: "--font-baloo", 
+  variable: "--font-baloo",
   subsets: ["latin"],
   weight: ["400", "700"],
 });
@@ -12,7 +13,7 @@ const baloo = Baloo_2({
 export const metadata = {
   title: "Toiture Marseille",
   description: "Votre expert de la toiture à Marseille",
-   icons: {
+  icons: {
     icon: "/favicon.ico",
   },
 };
@@ -20,7 +21,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head />
       <body className={`${baloo.variable}`}>
+        {/* ✅ Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17547161127"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17547161127');
+          `}
+        </Script>
+
         <Header />
         {children}
         <Footer />
