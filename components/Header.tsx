c2ef3c961a10103e012601c06deb259f18104527
Codev <codev@codevs-air.home>
2026-06-13 12:28:03 +0200

    mise en ligne

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
       
       {/* LOGO INJECTÉ EN SVG DIRECT */}
       <Link href="/" className="flex items-center transition-transform duration-200 hover:scale-105">       
-        <Logo width={150} height={150} className="w-[150px] h-[150px]" />        
+        <Logo width={140} height={140} className="w-[140px] h-[140px]" />        
       </Link>
 
       {/* MENU DESKTOP (Au survol : passage au bleu ciel) */}
